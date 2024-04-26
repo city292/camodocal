6b2f1247f53db44dd7200615d2617adf1e1927ba
citianyu <citianyu.syq@aohua.com>
2024-04-26 23:10:22 +0800

    init

--- build.sh
+++ build.sh
@@ -16,3 +16,7 @@ make install
 
 
 COMMENT
+
+
+
+
--- stereo_calib.sh
+++ stereo_calib.sh
@@ -1,9 +1,9 @@
 /data/project/camodocal/install/bin/stereo_calib \
--i  /data/endo_data/3D_endo/0326-采图/rgb \
--o /data/endo_data/3D_endo/0326-采图/rgb/mei_out \
--e .png \
+-i  /data/endo_data/3D_endo/0417-采图 \
+-o /data/endo_data/3D_endo/0417-采图/mei_out \
+-e .bmp \
 -s 5.2 \
 -w 11 -h 8 \
---camera-model mei
-
+--camera-model mei \
+--opencv 
 
